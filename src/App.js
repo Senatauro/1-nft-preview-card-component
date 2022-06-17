b04d6cd182506363b5d0bbcd14574e0c6d055caf
Senatauro <cgsste@hotmail.com>
2022-06-17 11:29:42 -0300

    minor update
    Adding more meaningful img alt attributes

--- src/App.js
+++ src/App.js
@@ -9,23 +9,23 @@ function App() {
   return (
     <div className='container'>
       <div className="card-container">
-        <img className='card-image' src={nftImage}/>
+        <img className='card-image' src={nftImage} alt="The Equilibrium" />
         <h1 className='card-title'>Equilibrium #3429</h1>
         <p className='card-desc'>Our Equilibrium collection promotes balance and calm.</p>
         <div className='card-info'>
           <div className='card-info-price'>
-            <img src={nftPrice} alt="nft-price" />
+            <img src={nftPrice} alt="Ether icon" />
             <p>0.041 ETH</p>
           </div>
           <div className='card-info-time'>
-            <img src={nftTime}/>
+            <img src={nftTime} alt="Time icon" />
             <p>3 days left</p>
           </div>
         </div>
         <div className='card-footer'>
           <hr />
           <div className='card-footer-creator'>
-            <img src={nftCreator}/>
+            <img src={nftCreator} alt="Photo of the NFT creator" />
             <p>Creation of <span>Jules Wyvern</span></p>
           </div>
         </div>
